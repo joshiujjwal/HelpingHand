0738e5d503dc28066e526cc4efb80de85cc9512e
Ujjwal <ujjwaljoshi93@gmail.com>
2019-11-22 17:35:16 -0500

    moved signup/login login to users app

--- helpinghand-project/helpinghand/urls.py
+++ helpinghand-project/helpinghand/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
-from course import views
+from user import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
