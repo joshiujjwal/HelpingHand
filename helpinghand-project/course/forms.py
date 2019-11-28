d755b34b9e9f3b0501ea13796485bd6b30961bc8
Ujjwal <ujjwaljoshi93@gmail.com>
2019-11-28 15:01:46 -0500

    Student can unenroll from the course

--- helpinghand-project/course/forms.py
+++ helpinghand-project/course/forms.py
@@ -1,9 +1,11 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, IntegerField
 from .models import Course
 from django.db import transaction
 from user.models import UserProfile
+from django import forms
 
-class EnrollForm(ModelForm):
+
+class EnrollForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(EnrollForm, self).__init__(*args, **kwargs)
         self.fields['name'].widget.attrs['readonly'] = True
@@ -11,3 +13,4 @@ class EnrollForm(ModelForm):
     class Meta:
         model = Course
         fields = ['name',]
+    course_id = forms.IntegerField(widget = forms.HiddenInput(), required = False)
