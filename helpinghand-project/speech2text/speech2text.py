ca626f57c7b6b3c6f90d60f22bb670b3ced15d44
Snehal Bedmutha <snehal29@terpmail.umd.edu>
2019-11-30 11:22:44 -0800

    updated instruction and code

--- helpinghand-project/speech2text/speech2text.py
+++ helpinghand-project/speech2text/speech2text.py
@@ -6,6 +6,9 @@ from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
 from youtube_transcript_api import YouTubeTranscriptApi
+import dequeue
+
+
 
 video_id_list ={"knitting":'PLYfCBK8IplO6v0QjCj-TSrFUXnRV0WxfE'}
 for id,value in video_id_list.items():
@@ -15,4 +18,7 @@ for id,value in video_id_list.items():
 for i in dict:
     file.write(i["text"]+"\n")
 file.close()
+
+video_id_list = {}
+
 
