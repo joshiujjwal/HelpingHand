bc972f7f6470a6c93eaa656eb181c218154b6ca8
Snehal Bedmutha <snehal29@terpmail.umd.edu>
2019-11-30 02:24:00 -0800

    speech2text api is ready

---
+++ helpinghand-project/speech2text/speech2text.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+
+import os
+
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+from youtube_transcript_api import YouTubeTranscriptApi
+
+video_id_list ={"knitting":'PLYfCBK8IplO6v0QjCj-TSrFUXnRV0WxfE'}
+for id,value in video_id_list.items():
+    dict =YouTubeTranscriptApi.get_transcript("sF_jSrBhdlg&list="+value)
+    file = open(r"transcript"+id+".txt", "a+")
+
+for i in dict:
+    file.write(i["text"]+"\n")
+file.close()
+
